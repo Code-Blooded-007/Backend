350e211c4d7cde020e22b22c02fe80260690bfb6
Aman Roy <royaman8757@gmail.com>
2019-02-25 11:00:16 +0530

    more fake data added to websites

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,9 @@ from app import app
 def index():
 	websites = [
 		{'name' : 'google.com', 'status' :'active'},
-		{'name' : 'ghrcem.net', 'status' : 'not active'}
+		{'name' : 'ghrcem.net', 'status' : 'active'},
+		{'name' : 'mis.com', 'status' : 'not active'},
+		{'name' : 'github.com', 'status' : 'active'},
+		{'name' : 'w3school.com', 'status' : 'active'}
 	]
 	return render_template('index.html', title='Home', websites=websites)
