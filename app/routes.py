14330336c1db35f0db6757ad5f04d65d8cb21071
Aman Roy <royaman8757@gmail.com>
2019-02-25 09:14:26 +0530

    bug fixed:indentation

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,8 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	websites = {
-		'google.com' : 'active',
-		'ghrcem.net' : 'not active'
-	}
-    return render_template('index.html', title='Home', websites=websites)
+	websites = [
+		{'name' : 'google.com', 'status' :'active'},
+		{'name' : 'ghrcem.net', 'status' : 'not active'}
+	]
+	return render_template('index.html', title='Home', websites=websites)
